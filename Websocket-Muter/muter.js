5de541d327d4e1ca3ce50629f349912c77f333d8
Chris Angelico <rosuav@gmail.com>
2019-09-06 21:21:32 +1000

    Autounmute only those tabs that were automuted.
    This was originally the intention of the "was this muted by this extension"
    check, but now that Ctrl-M is the normal way to mute a single tab (thanks,
    Chrome, for removing that from the right-click menu), it's too easy to
    accidentally unmute too many tabs.

--- Websocket-Muter/muter.js
+++ Websocket-Muter/muter.js
@@ -7,11 +7,17 @@ console.log("Chrome:", chrome);
 
 //Map tab IDs to the time when they may again be automuted
 const nomute = {};
+//Map tab IDs to true if they were automuted, false if not
+//If absent from this mapping, assume false. Note that any
+//muting done outside of this extension should also imply
+//'false' here, but must be checked for separately.
+const automuted = {};
 function automute(tab)
 {
 	if (tab.audible && !tab.mutedInfo.muted && +new Date >= (nomute[tab.id]||0))
 	{
 		console.log("Noisy tab:", tab);
+		automuted[tab.id] = true;
 		chrome.tabs.update(tab.id, {"muted": true});
 	}
 }
@@ -19,7 +25,8 @@ function autounmute(tab)
 {
 	if (tab.mutedInfo.muted &&
 		tab.mutedInfo.reason === "extension" &&
-		tab.mutedInfo.extensionId === chrome.runtime.id)
+		tab.mutedInfo.extensionId === chrome.runtime.id &&
+		automuted[tab.id])
 	{
 		console.log("Muted tab:", tab);
 		chrome.tabs.update(tab.id, {"muted": false});
@@ -27,10 +34,12 @@ function autounmute(tab)
 }
 function togglemute(tab)
 {
+	automuted[tab.id] = false;
 	chrome.tabs.update(tab.id, {"muted": !tab.mutedInfo.muted});
 }
 function keep(tab)
 {
+	automuted[tab.id] = false;
 	chrome.tabs.update(tab.id, {"muted": false}); //Force it to be unmuted
 	nomute[tab.id] = 30000 + +new Date; //30,000 ms or 30 seconds
 }
@@ -55,7 +64,8 @@ function setup_socket() {
 	socket.onopen = () => console.log("Socket connection established.");
 	socket.onmessage = ev => {
 		const msg = JSON.parse(ev.data);
-		if (msg && msg.type === "quiet") alltabs(msg.data ? mute : unmute);
+		console.log("WS:", msg);
+		if (msg && msg.type === "quiet") alltabs(msg.data ? automute : autounmute);
 	};
 	socket.onclose = () => {socket = null; setTimeout(setup_socket, 5000);};
 }
