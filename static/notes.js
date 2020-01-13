0c79dc0f3d4e354543245240eca800dd1569f765
Chris Angelico <rosuav@gmail.com>
2020-01-13 11:08:29 +1100

    Show some info about the recordings in this block

--- static/notes.js
+++ static/notes.js
@@ -1,14 +1,33 @@
 import choc, {set_content} from "https://rosuav.github.io/shed/chocfactory.js";
-const {P, AUDIO} = choc;
+const {AUDIO, B, DETAILS, LI, P, PRE, SUMMARY} = choc;
 
+const block = parseInt(window.location.hash.substr(1), 10); //If NaN, we don't have block info
+
+function render_recording(rec) {
+	return LI(DETAILS([
+		SUMMARY([B(rec.google), " " + rec.desc]),
+		PRE(rec.sphinx.join("\n")),
+		rec.spec[0] && P(`Spectating ${rec.spec[0]} (${rec.spec[1]})`),
+		rec.time && P(`At ${rec.time.toFixed(1)}s`),
+	]));
+}
+
+let metadata = {};
+function update_meta(newmeta) {
+	metadata = newmeta;
+	if (!metadata.recordings) metadata.recordings = [];
+	const ul = document.getElementById("recordings");
+	if (ul.children.length === metadata.recordings.length) return;
+	for (let i = ul.children.length; i < metadata.recordings.length; ++i)
+		ul.appendChild(render_recording(metadata.recordings[i]));
+}
 //set_content("p", AUDIO({controls: true, src: "/recordings/6/02 - R1 (0::0) spec-2-Gunner.flac"}));
-//fetch("/recordings/6/metadata.json").then(r => r.json()).then(r => console.log(r))
 
 const protocol = window.location.protocol == "https:" ? "wss://" : "ws://";
 let socket;
 function init_socket() {
 	socket = new WebSocket(protocol + window.location.host + "/ws");
-	socket.onopen = () => {socket.send(JSON.stringify({type: "init", block: 3}));};
+	socket.onopen = () => {socket.send(JSON.stringify({type: "init", block}));};
 	socket.onmessage = (ev) => {
 		const msg = JSON.parse(ev.data);
 		switch (msg.type) {
@@ -20,4 +39,7 @@ function init_socket() {
 	//Automatically reconnect (after a one-second delay to prevent spinning)
 	socket.onclose = ev => setTimeout(init_socket, 1000);
 }
-init_socket();
+if (block) {
+	init_socket();
+	fetch("/recordings/" + block + "/metadata.json").then(r => r.json()).then(r => update_meta(r));
+}
